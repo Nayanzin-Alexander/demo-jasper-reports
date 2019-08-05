d2b9ca2c5d578f7161d83d71da02f6371e5fa97b
Nayanin Alexander <nayanzin.alexander@gmail.com>
2019-08-05 16:23:00 +0300

    Initial commit

---
+++ src/main/java/com/nayanzin/demojasperreports/DemoJasperReportsApplication.java
@@ -0,0 +1,13 @@
+package com.nayanzin.demojasperreports;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoJasperReportsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoJasperReportsApplication.class, args);
+    }
+
+}
